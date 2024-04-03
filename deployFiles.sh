b7ff31dec0c9b896221b97ab43383c4af8796de7
Harley Christensen <50967934+magorkel@users.noreply.github.com>
2024-04-03 10:48:28 -0600

    changes

--- deployFiles.sh
+++ deployFiles.sh
@@ -9,7 +9,7 @@ do
     esac
 done
 
-if [[ -z "$key" || -z "$ballet260" || -z "$service" ]]; then
+if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
     printf "\nMissing required parameter.\n"
     printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
     exit 1
@@ -19,11 +19,11 @@ printf "\n----> Deploying files for $service to $hostname with $key\n"
 
 # Step 1
 printf "\n----> Clear out the previous distribution on the target.\n"
-ssh -i "$key" ubuntu@$ballet260 << ENDSSH
+ssh -i "$key" ubuntu@$hostname << ENDSSH
 rm -rf services/${service}/public
 mkdir -p services/${service}/public
 ENDSSH
 
 # Step 2
 printf "\n----> Copy the distribution package to the target.\n"
-scp -r -i "$key" * ubuntu@$ballet260:services/$service/public
+scp -r -i "$key" * ubuntu@$hostname:services/$service/public
